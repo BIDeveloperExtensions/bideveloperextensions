489fc2474e3a7534ff2e8780a7394ecd64c13f16
dgosbell_cp <SND\dgosbell_cp>
2008-03-31 09:59:05 +0000

    Updating versions for the 1.3.0.0 Beta release

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.2.0.1")]
+[assembly: AssemblyVersion("1.3.0.0")]
 [assembly: ComVisible(true)]
 [assembly: NeutralResourcesLanguageAttribute("")]
-[assembly: AssemblyFileVersionAttribute("1.2.0.1")]
+[assembly: AssemblyFileVersionAttribute("1.3.0.1")]
