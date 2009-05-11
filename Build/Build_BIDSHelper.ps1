ebb9ded1d84e0d5b7bc35def2a76f59bd1a2040f
dgosbell_cp <SND\dgosbell_cp>
2009-05-11 12:53:10 +0000

    Version 1.4.1.0

--- Build/Build_BIDSHelper.ps1
+++ Build/Build_BIDSHelper.ps1
@@ -1,63 +1,44 @@
-properties{
-    #$newVersion = "1.4.0.1"
-    $newVersion = $ReleaseVersion
-}
+param(
+    [string]$ReleaseVersion = ""
+)
 
-properties{#directories
-    #$framework_dir_2 = "$env:systemroot\microsoft.net\framework\v2.0.50727"
-    #$framework_dir_3_5 = "$env:systemroot\microsoft.net\framework\v3.5"
-    $base_dir = [System.IO.Directory]::GetParent("$pwd")
-    $build_dir = "$base_dir\build"
-}
+#$framework_dir_2 = "$env:systemroot\microsoft.net\framework\v2.0.50727"
+$framework_dir_3_5 = "$env:systemroot\microsoft.net\framework\v3.5"
+$base_dir = [System.IO.Directory]::GetParent("$pwd")
+$build_dir = "$base_dir\build"
+$sln_file_2005 = "$base_dir\vs2008_bidshelper.sln"
+$sln_file_2008 = "$base_dir\SQL2008_bidshelper.sln"
 
-properties { #solution file
-    $sln_file_2005 = "$base_dir\vs2008_bidshelper.sln"
-    $sln_file_2008 = "$base_dir\SQL2008_bidshelper.sln"
-    #$sql_ver = "2005"
-}
+#utility locations
+$msbuild = "$framework_dir_3_5\msbuild.exe"  
+$tf = "$env:ProgramFiles\Microsoft Visual Studio 9.0\Common7\IDE\tf.exe"
+$nsisPath = "$env:ProgramFiles\NSIS\makensis.exe"
+$zip = "$env:ProgramFiles\7-zip\7z.exe"
 
-
-properties { #utility locations
-    $msbuild = "msbuild.exe"  
-    $tf = "$env:ProgramFiles\Microsoft Visual Studio 9.0\Common7\IDE\tf.exe"
-    $nsisPath = "$env:ProgramFiles\NSIS\makensis.exe"
-    $zip = "$env:ProgramFiles\7-zip\7z.exe"
-}
-
-
-properties { #version files
-    $versionFiles = @("$base_dir\assemblyinfo.cs", "ascii"),
+#version files
+$versionFiles = @("$base_dir\assemblyinfo.cs", "ascii"),
         @("$base_dir\setupScript\BIDSHelperSetup.nsi", "ascii"),
         @("$base_dir\setupScript\BIDSHelperSetup2008.nsi", "ascii"),
         @("$base_dir\setupScript\SQL2005CurrentReleaseVersion.xml", "UTF8"),
         @("$base_dir\setupScript\SQL2008CurrentReleaseVersion.xml", "UTF8"),
         @("$base_dir\BIDSHelper.Addin", "unicode"),
         @("$base_dir\BIDSHelper2008.Addin", "unicode")
-    $lastReleaseFile = "$base_dir\setupScript\SQL2005CurrentReleaseVersion.xml"
-}
+$lastReleaseFile = "$base_dir\setupScript\SQL2005CurrentReleaseVersion.xml"
 
+#Clean 
 
-task default -depends CheckinVersionFiles
-
-task Clean{ 
-    Write-Host "Executed Clean!"
-}
-
-task checkVersion {
-    if($newVersion.Length -eq 0)
+#checkVersion 
+    if($ReleaseVersion.Length -eq 0)
     {
         # if we have not been given an explicit build, then we increment the build number
         # of the current release by 1.
         [string]$(get-Content "$($lastReleaseFile)") -cmatch '\d+\.\d+\.\d+\.\d+'
         $ver = $matches[0]
-        $newVersion = $ver -replace "(?'main'\d+\.\d+\.\d+\.)(?'build'\d+)", "$($matches['main'])$([int]$matches['build']+1)"
+        $ReleaseVersion = $ver -replace "(?'main'\d+\.\d+\.\d+\.)(?'build'\d+)", "$($matches['main'])$([int]$matches['build']+1)"
     }
     
-    
-}
-
-task updateVersion -depends checkVersion {
-    write-Host $newVersion
+# updateVersion 
+    write-Host $ReleaseVersion
     
     $user = read-Host "Codeplex UserName"
     $pass =  read-Host "Codeplex Password" -assecurestring
@@ -77,53 +58,43 @@ task updateVersion -depends checkVersion {
     }
     
     $versionFiles | foreach-Object {
-        $newContent =  $(get-Content "$($_[0])") -replace "\d+\.\d+\.\d+\.\d+", $newVersion
+        $newContent =  $(get-Content "$($_[0])") -replace "\d+\.\d+\.\d+\.\d+", $ReleaseVersion
         set-Content $_[0] $newContent -encoding $_[1]
     }
-}
 
-
-
-task Compile -depends updateVersion, Clean {
+#Compile
     write-Host "Path: $msbuild"
     &($msbuild) $sln_file_2005 /t:Rebuild /p:Configuration=Release /v:q
     Write-Host "Executed Compile!"
-}
 
-task BuildSetup -depends Compile {
+#BuildSetup 
     write-Host "Starting NSIS"
     &($nsisPath) "$base_dir\SetupScript\BIDSHelperSetup.nsi"
     write-Host "Completed NSIS"
-}
 
-task BuildXCopy -depends Compile {
+#BuildXCopy 
     write-Host "Starting Zip"
-    $ver = $newVersion -replace "\.", "_"
+    $ver = $ReleaseVersion -replace "\.", "_"
     &($zip) a -tzip "$base_dir\SetupScript\BIDSHelper2005_$ver.zip" "$base_dir\bin\BIDSHelper.dll"
     &($zip) a -tzip "$base_dir\SetupScript\BIDSHelper2005_$ver.zip" "$base_dir\BIDSHelper.Addin"
-}
 
-task Compile2008 -depends updateVersion, Clean, BuildXCopy,BuildSetup { # the 2008 release depends on the 2005 release so they get built sequentially
+#Compile2008 
 write-Host "Path:  $msbuild"
     &($msbuild) $sln_file_2008 /t:Rebuild /p:Configuration=Release /v:q
     Write-Host "Executed Compile!"
-}
 
-task BuildSetup2008 -depends Compile2008 {
+#BuildSetup2008
     write-Host "Starting NSIS"
     &($nsisPath) "$base_dir\SetupScript\BIDSHelperSetup2008.nsi"
     write-Host "Completed NSIS"
-}
 
-task BuildXCopy2008 -depends Compile2008 {
+#BuildXCopy2008
     write-Host "Starting Zip"
-    $ver = $newVersion -replace "\.", "_"
+    $ver = $ReleaseVersion -replace "\.", "_"
     &($zip) a -tzip "$base_dir\SetupScript\BIDSHelper2008_$ver.zip" "$base_dir\bin\BIDSHelper.dll"
     &($zip) a -tzip "$base_dir\SetupScript\BIDSHelper2008_$ver.zip" "$base_dir\BIDSHelper2008.Addin"
-}
 
-
-task CheckinVersionFiles -depends BuildSetup, BuildXCopy, BuildSetup2008, BuildXCopy2008 {
+# CheckinVersionFiles
     $cont = read-host "
 Release Files Built!    
 ====================
@@ -138,13 +109,11 @@ Just hit Enter to abort without updating the source repository."
         write-Host "checking files into codeplex and applying release label"
         $versionFiles | foreach-Object {
             write-Host "checking out: $($_[0])"
-            &($tf) checkin "/login:$user,$pass" "/comment:`"Updating to version $newVersion`"" "$($_[0])"
-            write-host ($tf) checkin "/login:$user,$pass" "/comment:`"Updating to version $newVersion`"" "$($_[0])"
+            &($tf) checkin "/login:$user,$pass" "$($_[0])" "/comment:`"Updating to version $ReleaseVersion`""
         }   
-        &($tf) label "`"Release $newVersion`"" "/login:$user,$pass" /v:T /recursive *.*
+        &($tf) label "`"Release $ReleaseVersion`"" *.* "/login:$user,$pass" /v:T /recursive
     }
     else
     {
         write-Host "No Files checked-in"
     }
-}
