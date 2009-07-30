c4330a64b091649cd3b3627ebbf72f3b39253cc1
dgosbell_cp <SND\dgosbell_cp>
2009-07-30 22:00:30 +0000

    Commenting out unused reference to System.Linq

--- SSIS/PackageHelper.cs
+++ SSIS/PackageHelper.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using System.Text;
 using Microsoft.SqlServer.Dts.Pipeline.Wrapper;
 using Microsoft.SqlServer.Dts.Runtime;
