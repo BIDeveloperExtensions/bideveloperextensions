04360022f3f8b0889eb9f63dc827143dfffa9d8f
furmangg_cp <SND\furmangg_cp>
2009-11-17 18:46:41 +0000

    Perspectives tab highlighting tweaks tweaking highlighting logic for KPIs adding highlighting for Actions and calculated members

--- SSAS/TriStatePerspectivesPlugin.cs
+++ SSAS/TriStatePerspectivesPlugin.cs
@@ -112,6 +112,8 @@ namespace BIDSHelper
                     //ArrayList columns = (ArrayList)storage.GetType().BaseType.InvokeMember("m_arrColumns", getfieldflags, null, storage, new object[] { });
                     object[] perspectivesColumns = (object[])rows[0];
 
+                    ArrayList allGridCubeObjects = (ArrayList)perspectiveBuilder.GetType().InvokeMember("allGridCubeObjects", getfieldflags, null, perspectiveBuilder, null);
+
                     for (int i = 3; i < rows.Count; i++)
                     {
                         object[] columns = (object[])rows[i];
@@ -242,6 +244,142 @@ namespace BIDSHelper
                                 }
                             }
                         }
+                        else if (rowObjectType.EndsWith("Action"))
+                        {
+                            cell = columns[1];
+                            string rowObjectName = (string)cell.GetType().InvokeMember("CellData", getpropertyflags, null, cell, null);
+                            rowObjectName = rowObjectName.Trim();
+                            Microsoft.AnalysisServices.Action action = cube.Actions.GetByName(rowObjectName);
+                            for (int j = 3; j < columns.Length; j += 2)
+                            {
+                                cell = perspectivesColumns[j + 1];
+                                string perspectiveName = (string)cell.GetType().InvokeMember("CellData", getpropertyflags, null, cell, null);
+                                Perspective perspective = cube.Perspectives.GetByName(perspectiveName);
+                                cell = columns[j];
+                                bool bHighlight = false;
+                                if (perspective.Actions.Contains(action.ID))
+                                {
+                                    bHighlight = true;
+                                    if (action.TargetType == ActionTargetType.DimensionMembers)
+                                    {
+                                        foreach (PerspectiveDimension dim in perspective.Dimensions)
+                                        {
+                                            if (string.Compare(action.Target, "[" + dim.CubeDimension.Name + "]", true) == 0)
+                                            {
+                                                bHighlight = false;
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    else if (action.TargetType == ActionTargetType.AttributeMembers || action.TargetType == ActionTargetType.HierarchyMembers)
+                                    {
+                                        foreach (PerspectiveDimension dim in perspective.Dimensions)
+                                        {
+                                            foreach (PerspectiveAttribute attr in dim.Attributes)
+                                            {
+                                                if (string.Compare(action.Target, "[" + attr.Parent.CubeDimension.Name + "].[" + attr.Attribute.Name + "]", true) == 0)
+                                                {
+                                                    bHighlight = false;
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                    }
+                                    else if (action.TargetType == ActionTargetType.Level || action.TargetType == ActionTargetType.LevelMembers)
+                                    {
+                                        foreach (PerspectiveDimension dim in perspective.Dimensions)
+                                        {
+                                            foreach (PerspectiveHierarchy hier in dim.Hierarchies)
+                                            {
+                                                foreach (Level level in hier.Hierarchy.Levels)
+                                                {
+                                                    if (string.Compare(action.Target, "[" + hier.Parent.CubeDimension.Name + "].[" + hier.Hierarchy.Name + "].[" + level.Name + "]", true) == 0)
+                                                    {
+                                                        bHighlight = false;
+                                                        break;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                    else if (action.TargetType == ActionTargetType.HierarchyMembers)
+                                    {
+                                        foreach (PerspectiveDimension dim in perspective.Dimensions)
+                                        {
+                                            foreach (PerspectiveHierarchy hier in dim.Hierarchies)
+                                            {
+                                                if (string.Compare(action.Target, "[" + hier.Parent.CubeDimension.Name + "].[" + hier.Hierarchy.Name + "]", true) == 0)
+                                                {
+                                                    bHighlight = false;
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        bHighlight = false;
+                                    }
+                                }
+
+                                System.Reflection.BindingFlags setpropertyflags = System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.SetProperty | System.Reflection.BindingFlags.DeclaredOnly | System.Reflection.BindingFlags.Instance;
+                                System.Drawing.SolidBrush brush = null;
+                                if (bHighlight)
+                                {
+                                    brush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+                                }
+                                else
+                                {
+                                    brush = (System.Drawing.SolidBrush)columns[j + 1].GetType().InvokeMember("BkBrush", getpropertyflags, null, columns[j + 1], null);
+                                }
+                                lock (cell)
+                                {
+                                    cell.GetType().InvokeMember("BkBrush", setpropertyflags, null, cell, new object[] { brush });
+                                }
+                            }
+                        }
+                        else if (rowObjectType == "CalculatedMember")
+                        {
+                            cell = columns[1];
+                            string rowObjectName = (string)cell.GetType().InvokeMember("CellData", getpropertyflags, null, cell, null);
+                            rowObjectName = rowObjectName.Trim();
+
+                            for (int j = 3; j < columns.Length; j += 2)
+                            {
+                                cell = perspectivesColumns[j + 1];
+                                string perspectiveName = (string)cell.GetType().InvokeMember("CellData", getpropertyflags, null, cell, null);
+                                Perspective perspective = cube.Perspectives.GetByName(perspectiveName);
+                                cell = columns[j];
+
+                                Microsoft.AnalysisServices.Design.Script calc = (Microsoft.AnalysisServices.Design.Script)allGridCubeObjects[i].GetType().InvokeMember("Object", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.GetField, null, allGridCubeObjects[i], null);
+                                if (CalcIsHidden(calc)) continue;
+                                                                
+                                bool bHighlight = false;
+                                if (calc != null && calc.CalculationProperty != null && perspective.Calculations.Contains(calc.CalculationProperty.CalculationReference) && !CalcIsHidden(calc))
+                                {
+                                    if (!string.IsNullOrEmpty(calc.CalculationProperty.AssociatedMeasureGroupID))
+                                    {
+                                        if (!perspective.MeasureGroups.Contains(calc.CalculationProperty.AssociatedMeasureGroupID))
+                                            bHighlight = true;
+                                    }
+                                }
+
+                                System.Reflection.BindingFlags setpropertyflags = System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.SetProperty | System.Reflection.BindingFlags.DeclaredOnly | System.Reflection.BindingFlags.Instance;
+                                System.Drawing.SolidBrush brush = null;
+                                if (bHighlight)
+                                {
+                                    brush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+                                }
+                                else
+                                {
+                                    brush = (System.Drawing.SolidBrush)columns[j + 1].GetType().InvokeMember("BkBrush", getpropertyflags, null, columns[j + 1], null);
+                                }
+                                lock (cell)
+                                {
+                                    cell.GetType().InvokeMember("BkBrush", setpropertyflags, null, cell, new object[] { brush });
+                                }
+                            }
+                        }
                     }
                     grid.Refresh();
                 }
@@ -257,11 +395,10 @@ namespace BIDSHelper
             || ShouldPerspectiveKpiColumnBeHighlighted(pkpi, mdxScript, pkpi.Kpi.Trend);
         }
 
-        //return true if the kpi column is a reference to a measure, and if that measure isn't in the perspective
+        //return true if the kpi column is a reference to a measure, and if that measure isn't in the perspective or is hidden
         private bool ShouldPerspectiveKpiColumnBeHighlighted(PerspectiveKpi pkpi, Microsoft.AnalysisServices.Design.Scripts mdxScript, string column)
         {
             if (string.IsNullOrEmpty(column)) return false;
-            if (pkpi.Parent.Calculations.Contains(column)) return false;
             foreach (MeasureGroup mg in pkpi.ParentCube.MeasureGroups)
             {
                 foreach (Measure m in mg.Measures)
@@ -276,16 +413,27 @@ namespace BIDSHelper
                 }
             }
 
-            foreach (object calcMember in mdxScript.CalculatedMembers)
-            {
-                Microsoft.AnalysisServices.Design.Script calc = calcMember as Microsoft.AnalysisServices.Design.Script;
-                if (calc != null && calc.CalculationProperty != null && string.Compare(calc.CalculationProperty.CalculationReference, column, true) == 0)
-                {
-                    //found the calc, but it isn't in the perspective
-                    return true;
-                }
-            }
+            //foreach (object calcMember in mdxScript.CalculatedMembers)
+            //{
+            //    Microsoft.AnalysisServices.Design.Script calc = calcMember as Microsoft.AnalysisServices.Design.Script;
+            //    if (calc != null && calc.CalculationProperty != null && string.Compare(calc.CalculationProperty.CalculationReference, column, true) == 0)
+            //    {
+            //        if (CalcIsHidden(calc)) return false;
+            //        if (pkpi.Parent.Calculations.Contains(column))
+            //            return false;
+            //        else //found the calc, it's visible, but it isn't in the perspective
+            //            return true;
+            //    }
+            //}
             return false;
+        }
+
+        public static bool CalcIsHidden(Microsoft.AnalysisServices.Design.Script calc)
+        {
+            //calc.CalculationProperty.Visible doesn't seem to work
+            object mdxCodeCalc = calc.GetType().InvokeMember("mdxCodeCalc", System.Reflection.BindingFlags.GetField | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance, null, calc, null);
+            bool bIsHidden = (bool)mdxCodeCalc.GetType().InvokeMember("IsHidden", System.Reflection.BindingFlags.GetProperty | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public, null, mdxCodeCalc, null);
+            return bIsHidden;
         }
 
         void grid_KeyPress(object sender, KeyPressEventArgs e)
