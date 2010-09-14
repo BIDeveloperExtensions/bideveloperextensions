2a13b49c19423379880ea7e771ffaf5b0e82348c
DarrenSQLIS_cp <SND\DarrenSQLIS_cp>
2010-09-14 09:26:16 +0000

    Moved AssemblyInfo, and added conditional descriptions for 2005 vs 2008. Removed bad app.config file reference in 2008 project.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("BIDS Helper")]
+[assembly: AssemblyDescription("Provides additional useful features to the BI Development Studio")]
+[assembly: AssemblyCompany("http://bidshelper.codeplex.com/")]
+[assembly: AssemblyCopyright("Copyright © 2010 BIDS Helper")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.4.3.1")]
+[assembly: AssemblyFileVersion("1.4.3.1")]
+
+[assembly: ComVisible(true)]
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+#if KATMAI
+[assembly: AssemblyTitle("BIDS Helper for SQL Server 2008")]
+#else
+[assembly: AssemblyTitle("BIDS Helper for SQL Server 2005")]
+#endif
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
