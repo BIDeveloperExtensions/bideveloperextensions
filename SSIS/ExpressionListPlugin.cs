f3f30daaa1871f3da88dbce2ffc1f4457eda57e9
furmangg_cp <SND\furmangg_cp>
2012-02-03 04:31:20 +0000

    preventing Expression List from always popping up

--- SSIS/ExpressionListPlugin.cs
+++ SSIS/ExpressionListPlugin.cs
@@ -93,7 +93,8 @@ namespace BIDSHelper.SSIS
             toolWindow.SetTabPicture(icon); 
 #endif
 
-            toolWindow.Visible = true;
+            if (windowIsVisible)
+                toolWindow.Visible = true;
         }
 
         void expressionListWindow_EditExpressionSelected(object sender, EditExpressionSelectedEventArgs e)
