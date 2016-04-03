e7794e616fc27013668c063acb797e1576135403
Darren Gosbell <dgosbell@users.noreply.github.com>
2016-04-03 10:02:39 +0000

    Second update of Options page

---
+++ Core/VersionInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BidsHelper.Core
+{
+    public class VersionInfo
+    {
+        public const string Version = "1.7.0.1";
+
+    }
+}
