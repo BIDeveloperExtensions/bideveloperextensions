46196414882dfc78469c4f90dad34375e93844c9
furmangg_cp <SND\furmangg_cp>
2017-05-15 22:04:03 +0000

    finalizing report fix code improving SQL2016 and SQL2017 switching code to include registry

--- core/ReportViewerForm.cs
+++ core/ReportViewerForm.cs
@@ -32,9 +32,7 @@ namespace BIDSHelper
 
 
             //prevents a crash or error in VS2015
-            PermissionSet permissions = new PermissionSet(PermissionState.None);
-            permissions.AddPermission(new FileIOPermission(PermissionState.Unrestricted));
-            permissions.AddPermission(new SecurityPermission(SecurityPermissionFlag.Execution));
+            PermissionSet permissions = new PermissionSet(PermissionState.Unrestricted); 
             this.ReportViewerControl.LocalReport.SetBasePermissionsForSandboxAppDomain(permissions);
 
             AssemblyName asm_name = System.Reflection.Assembly.GetExecutingAssembly().GetName();
@@ -49,14 +47,7 @@ namespace BIDSHelper
                 this.ReportViewerControl.LocalReport.SetParameters(Parameters);
             }
 
-            this.ReportViewerControl.ReportError += ReportViewerControl_ReportError;
-
-
-            //this.ReportViewerControl.LocalReport.ExecuteReportInCurrentAppDomain(AppDomain.CurrentDomain.Evidence);
             this.ReportViewerControl.RefreshReport();
-            
-
-
         }
 
 
@@ -71,70 +62,8 @@ namespace BIDSHelper
             }
             catch { }
         }
-
-        private void ReportViewerControl_ReportError(object sender, Microsoft.Reporting.WinForms.ReportErrorEventArgs e)
-        {
-            MessageBox.Show("error!");
-        }
+        
     }
 
-
-
-    public class RDLCLocalReportProxy : MarshalByRefObject
-    {
-        //<remark> The LocalReport has confilct with xbap domain 
-        //then we create another domain just for load localreport
-        static AppDomain rdlcLocalReportDomain;
-        private static AppDomain RDLCLocalReportDomain
-        {
-            get
-            {
-                if (rdlcLocalReportDomain == null)
-                {
-                    var setup = new AppDomainSetup();
-                    setup.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
-                    setup.PrivateBinPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-                    rdlcLocalReportDomain = AppDomain.CreateDomain("BIDSHelperReportSandbox" + Guid.NewGuid().ToString().GetHashCode().ToString("x"), null, setup);
-                    //rdlcLocalReportDomain = AppDomainTools.CreateDomain("rdlcLocalReportDomain");
-                    rdlcLocalReportDomain.AssemblyResolve += new ResolveEventHandler(rdlcLocalReportDomain_AssemblyResolve);
-                }
-                return rdlcLocalReportDomain;
-            }
-        }
-
-        private static System.Reflection.Assembly rdlcLocalReportDomain_AssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            foreach (System.Reflection.Assembly ____ in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                if (
-                        string.Compare(____.GetName().Name, args.Name, true) == 0 ||
-                        string.Compare(____.FullName, args.Name, true) == 0
-                    )
-                    return ____;
-            }
-            return null;
-        }
-        public static ReportViewerForm GetReportViewerForm()
-        {
-            var proxy = (RDLCLocalReportProxy)RDLCLocalReportDomain.CreateInstanceAndUnwrap(typeof(RDLCLocalReportProxy).Assembly.FullName, typeof(RDLCLocalReportProxy).FullName);
-            return proxy.GetReportViewerFormPrivate();
-        }
-
-        private ReportViewerForm GetReportViewerFormPrivate()
-        {
-            return new ReportViewerForm();
-        }
-
-        //DataSet dsReportData;
-        //public void ShowRDLCReport(DataSet dsReportData, string reportFilePath, string mainReportName)//Stream rdlcReportDefstream)
-        //{
-        //    ReportViewerForm rptViewerForm = RDLCReportViewerFormProvider.Create();// create report viewer in current domain("rdlcLocalReportDomain")
-        //    GenerateLocalReport(dsReportData, reportFilePath, mainReportName,
-        //        rptViewerForm.ReportViewer.LocalReport);
-
-        //    rptViewerForm.ReportViewer.RefreshReport();
-        //    rptViewerForm.Open();//show report viewer in current domain("rdlcLocalReportDomain")
-
-        //}
-    }
+    
 }
