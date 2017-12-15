3bdd7ba246ae4f1b2abc4c7d1dbeb65037fe8973
furmangg <furmangg@users.noreply.github.com>
2017-12-15 01:13:50 -0600

    release 2.1.1

--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -9,7 +9,7 @@ namespace BIDSHelper.Core
     {
         // BIDS Helper Assembly & VSIX Version
         // N.B. Manually update the manifest file, if you change this - See source.extension.vsixmanifest
-        public const string Version = "2.1.0";
+        public const string Version = "2.1.1";
 
         private static readonly object lockResource = new object();
         private static Version visualStudioVersion;
